fbadd2042d6ca516fc2b6a81ca0515e6e6e0f103
Zulfiqar Mughul <zulfiqar.mughul@elev.ga.ntig.se>
2024-11-28 12:31:44 +0100

    first commit

---
+++ steg2/Program.cs
@@ -0,0 +1,48 @@
+﻿// string[] leksaker = { "Bil", "Docka", "Lego", "Teddy", "Boll" };
+
+// for (int i = 0; i < leksaker.Length; i++)
+// {
+// Console.WriteLine(leksaker[i]);
+// }
+
+// string[] klasskamrater = { "Brian", "wing", "Elias", "kamil", "ares"};
+
+// int[] värden = { 2, 5, 7, 1, 9 };
+
+// for (int i = 0; i < klasskamrater.Length; i++)
+// {
+// Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {betyg[i]}");
+// }
+
+
+// List<string> cities = new List<string>();
+
+
+// class Program
+// {
+// static void Main()
+// {
+// List<string> cities = new List<string>();
+// string input;
+// 
+// while (true)
+// {
+// Console.WriteLine("Ange en stad (eller skriv 'exit' för att avsluta):");
+// input = Console.ReadLine();
+// 
+// if (input.ToLower() == "exit")
+// break;
+// 
+// cities.Add(input);
+// Console.WriteLine($"'{input}' har lagts till i listan.");
+// }
+// 
+// Console.WriteLine("\nDu har lagt till följande städer:");
+// foreach (string city in cities)
+// {
+// Console.WriteLine(city);
+// }
+// }
+// }
+
+    
